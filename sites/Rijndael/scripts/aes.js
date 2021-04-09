d04cdd5aa65a48bec7e3ff53e8317bdb26ae3aef
Miguel Martín <alu0101209777@ull.edu.es>
2021-04-09 13:56:49 +0100

    setKey working

--- sites/Rijndael/scripts/aes.js
+++ sites/Rijndael/scripts/aes.js
@@ -6,21 +6,49 @@ this.byteMultiplier = new ByteMultiplier(0x1b);
 this.Nb = 4;
 
 this.state = new Array(4).fill(new Array(this.Nb));
+
+/**
+ * Receives a Number[] representing the initial text
+ * @param {Number[]} key 
+ * @returns A Number[][] square matrix taking input as columns. For example:
+ * Input: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+ * Output: [
+ *           [0, 4, 8,  12], 
+ *           [1, 5, 9,  13],
+ *           [2, 6, 10, 14],
+ *           [3, 7, 11, 15],
+ *         ]
+ */
 this.setKey = function(key) {
   // Creates a copy of key
   let settedKey = [];
-  for (let i = 0; i < 4; i++) {
-    settedKey.push(new Array(this.Nb));
-  }
-  
-  for(let i = 0, k = 0; i < this.Nb; i++, k++) {
-    for(let j = 0; j < 4; j++) {
-      settedKey[j][i] = key[k];
+  for (let i = 0, k = 0; i < 4; i++) {
+    settedKey.push([]);
+    for (let j = 0; j < this.Nb; j++) {
+      settedKey[i].push(key[k++])
     }
   }
-
+  this.transposeMatrix(settedKey);
   return settedKey;
 }
+
+/**
+ * Receives a matrix and transposes it inplace
+ * @param  matrix 
+ */
+this.transposeMatrix = function(matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i < j) {
+        let temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;  
+      }
+    }
+  }
+}
+
+
 this.key = [];
 // this.key = this.setKey(key);
 /**
